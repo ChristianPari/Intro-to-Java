43280a5b4c26a82ce55a8aac59c2073a02e54a6c
Christian Pari <ChristianPari@outlook.com>
2020-05-24 18:23:25 -0400

    Arguments and errors, how to run a program

--- Hello/src/MyFirstProgram.java
+++ Hello/src/MyFirstProgram.java
@@ -2,7 +2,13 @@ public class MyFirstProgram { // classes have names, 'public' (access modifier) 
 	// contents of this class are it's members
 	// current members: method 'main', String 'x'
 	// every Java program needs a 'public main method'
-	public static void main(String[] args) { // method
+	public static void main(String[] args) { // method, (arguments are within the parentheses)
+		// can pass in arguments to this program via terminal
+		// can access these arguments by accessing the arguments from this class (in parentheses)
+		//System.out.println("Hello " + args[0]);
+		//^ will only run in the terminal becuase that is where the argument was created
+		//^ a dynamic program
+		//^ not common!
 		// class method
 		// names can be different but 'main' is the runnable method
 		// 'static' no instance needed
@@ -14,6 +20,11 @@ public class MyFirstProgram { // classes have names, 'public' (access modifier) 
 		// 'println' is a method
 	}
 	
-//	String x = new String(); // example of a property, store values
+	//	String x = new String(); // example of a property, store values
+	
+	// ERROR TYPES
+	// compile errors - allows you to fix the error before it runs
+	// runtime errors - able to run the program but crashes
+	
 
 }
