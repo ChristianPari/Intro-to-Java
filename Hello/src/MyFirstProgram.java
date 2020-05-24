4984daad451851699b820bf65dbe08cf207fcd33
Christian Pari <ChristianPari@outlook.com>
2020-05-24 17:53:13 -0400

    Notes on basic syntax; class, methods, objects...

--- Hello/src/MyFirstProgram.java
+++ Hello/src/MyFirstProgram.java
@@ -1,7 +1,19 @@
-public class MyFirstProgram {
-
-	public static void main(String[] args) {
+public class MyFirstProgram { // classes have names, 'public' (access modifier) means can be accessed by other classes, methods in code
+	// contents of this class are it's members
+	// current members: method 'main', String 'x'
+	// every Java program needs a 'public main method'
+	public static void main(String[] args) { // method
+		// class method
+		// names can be different but 'main' is the runnable method
+		// 'static' no instance needed
+		// an 'object' is an instance of a class
+		// statements are contained within the methods; tell the computer to do something
 		System.out.println("Hello world!"); 
+		// 'System' is a class
+		// 'out' is a member of System
+		// 'println' is a method
 	}
+	
+//	String x = new String(); // example of a property, store values
 
 }
