f067d5a9bbbfb08839d9371983890bd11c8df611
Christian Pari <ChristianPari@outlook.com>
2020-05-25 11:20:57 -0400

    Data types, user input

--- Hello/src/MyFirstProgram.java
+++ Hello/src/MyFirstProgram.java
@@ -11,7 +11,7 @@ public class MyFirstProgram { // classes have names, 'public' (access modifier) 
 		// can pass in arguments to this program via terminal
 		// can access these arguments by accessing the arguments from this class (in parentheses)
 		
-		//System.out.println("Hello " + args[0]);
+//		System.out.println("Hello " + args[0]);
 		//^ will only run in the terminal becuase that is where the argument was created
 		//^ a dynamic program
 		//^ not common!
@@ -21,7 +21,8 @@ public class MyFirstProgram { // classes have names, 'public' (access modifier) 
 		// 'static' no instance needed
 		// an 'object' is an instance of a class
 		
-		// System.out.println("Hello world!");
+		// 1-2
+//		System.out.println("Hello world!");
 		// statements are contained within the methods; tell the computer to do something
 		// 'System' is a class
 		// 'out' is a member of System
@@ -30,18 +31,47 @@ public class MyFirstProgram { // classes have names, 'public' (access modifier) 
 		// STRUCTURE TO CREATE A NEW OBJECT
 		// type identifier = new type() () are the constructor of the class
 
+		// 3-5
 		// To get user input
-
-		System.out.println("What is your name?");
+//		System.out.println("What is your name?");
 		
-		Scanner scanner = new Scanner(System.in);
+//		Scanner scanner = new Scanner(System.in);
 		// without the import, would have to specify which type of Scanner is being used
 		// ex: java.util.Scanner scanner = new java.util.Scanner(System.in) much more verbose and not needed thanks to imports
 		// scanner is the variable name
 		// 'System.in' comes from the console
-		String name = scanner.nextLine(); // assigns the variable 'name' a String value that is from the console
+//		String name = scanner.nextLine(); // assigns the variable 'name' a String value that is from the console
 		
-		System.out.println("Hello " + name); // Outputs the concatenated string of "Hello" and the user inputed string
+//		System.out.println("Hello " + name); // Outputs the concatenated string of "Hello" and the user inputed string
+		
+		// 6
+		// Strings can be created without using the 'new' keyword
+		// basic:
+//		String basic = new String("This is a string");
+		// easier:
+//		String easier = "This is easier!"; A STRING LITERAL aka VALUE
+		// combining literals, variables => become expression
+		// expression evaluates to a value
+		
+		// 7
+		// Java is statically typed: all types for data is needed up front
+		// Data types: Primitive and Objects
+		// Primitive: booleans, bytes, char, short, long, int, float, double
+		// Object: instance of a class data type
+		
+//		int x = 5; // primitive
+//		Integer y = 5; // object (5 is autoboxed)
+		
+		// 8
+		// Scanner input
+//		Scanner scanner = new Scanner(System.in);
+//		String x = scanner.nextLine();
+//		System.out.println(x);
+		// the next thing scanned has to be a int or will throw an error
+//		int y = scanner.nextInt();
+//		System.out.println(y);
+//		int a = y + 10;
+//		System.out.println(a);
 		
 	}
 	
