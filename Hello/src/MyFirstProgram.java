fc3f20c2fe6c354b6fdd06cfc2c3a42dd24dbdd0
Christian Pari <ChristianPari@outlook.com>
2020-05-25 11:22:42 -0400

    Data types, user input

--- Hello/src/MyFirstProgram.java
+++ Hello/src/MyFirstProgram.java
@@ -1,4 +1,4 @@
-import java.util.Scanner; // builtin package from Java, needed for user input; keeps conflicts from happening
+import java.util.Scanner; // built-in package from Java, needed for user input; keeps conflicts from happening
 
 public class MyFirstProgram { // classes have names, 'public' (access modifier) means can be accessed by other classes, methods in code
 	
